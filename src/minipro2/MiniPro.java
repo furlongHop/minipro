fd040b1ff0383800d59fa5d9b15417bb1b0a48c4
황지우 <skyro13@naver.com>
2021-12-14 12:10:04 +0900

    miniProject

---
+++ src/minipro2/MiniPro.java
@@ -0,0 +1,126 @@
+package minipro2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MiniPro {
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		List<Person> pList = new ArrayList<Person>();
+		
+		Reader fr = new FileReader("C:\\javaStudy\\miniProject\\phoneDB.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		System.out.println("*******************************************");
+		System.out.println("*            전화번호 관리 프로그램              *");
+		System.out.println("*******************************************");
+		
+		
+		while(true) {
+			System.out.print("메뉴번호: ");
+			String num = sc.nextLine();
+			
+			if(num.equals("5")) {
+				System.out.println("*******************************************");
+				System.out.println("*                 감사합니다                  *");
+				System.out.println("*******************************************");
+				break;
+			}
+			
+			while(true) {
+				String list = br.readLine();
+				
+				if(list==null) {
+					break;
+				}
+				String[] pArray = list.split(",");
+				Person person = new Person(pArray[0],pArray[1],pArray[2]);
+				pList.add(person);
+			}
+			
+			switch(num) {
+			case "1" :
+				System.out.println("<1.리스트>");
+				listPrint(pList);
+				break;
+			case "2" :
+				System.out.println("<2.등록>");
+					
+				System.out.print(">이름: ");
+				String addName = sc.nextLine();
+				System.out.print(">휴대전화: ");
+				String addHp = sc.nextLine();
+				System.out.print(">회사전화: ");
+				String addCompany = sc.nextLine();
+				
+				Person friend = new Person(addName,addHp,addCompany);
+				pList.add(friend);
+				System.out.println("[등록되었습니다.]");
+				break;
+				
+			case "3" :
+				System.out.println("<3.삭제>");
+				System.out.print(">번호: ");
+				String removeNum = sc.nextLine();
+				
+				int rnum = Integer.parseInt(removeNum);
+				pList.remove(rnum-1);
+				
+				System.out.println("[삭제되었습니다.]");
+				break;
+				
+			case "4" :
+				System.out.println("<4.검색>");
+				System.out.print(">이름: ");
+				String nameSe = sc.nextLine();
+				
+				for(int i=0;i<pList.size();i++) {
+					if(pList.get(i).getName().contains(nameSe)) {
+						System.out.println(i+1+". "+pList.get(i).toString());
+					}
+				}
+				break;
+				
+			default :
+				System.out.println("[다시 입력해 주세요.]");	
+				break;
+			}//switch~case
+			System.out.println("");
+			System.out.println("1.리스트 2.등록 3.삭제 4.검색 5.종료");
+			System.out.println("-------------------------------------------");
+		}//while
+		
+	
+		Writer fw = new FileWriter("C:\\javaStudy\\miniProject\\phoneDB.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+	
+		for(int i=0;i<pList.size();i++) {
+			String str = pList.get(i).info();
+			bw.write(str);
+			bw.newLine();
+		}
+		
+		bw.close();
+		br.close();
+		
+		sc.close();
+
+	}
+
+	public static void listPrint(List<Person> personList) {
+		for(int i=0;i<personList.size();i++) {
+			System.out.println(i+1+". "+personList.get(i).getName()+" "+personList.get(i).getHp()+" "+personList.get(i).getCompany());
+		}
+	}
+}
---
+++ src/minipro2/Person.java
@@ -0,0 +1,58 @@
+package minipro2;
+
+public class Person {
+	
+	//필드
+	private String name;
+	private String hp;
+	private String company;
+	
+	//생성자
+	public Person() {
+		
+	}
+	
+	public Person(String name, String hp, String company) {
+		this.name = name;
+		this.hp = hp;
+		this.company = company;
+	}
+	
+	
+	//메소드 g/s
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	
+	//메소드 일반
+	@Override
+	public String toString() {
+		return name + " " + hp + " " + company;
+	}
+	
+	public String info() {
+		return name+","+hp+","+company;
+	}
+
+}
